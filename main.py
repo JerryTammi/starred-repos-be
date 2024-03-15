026aa1ace39e9556b95dc95bdddf0eaeb05cdb59
JerryTammi <33928862+JerryTammi@users.noreply.github.com>
2024-03-15 10:50:25 +0200

    Revert "Removed sessions and pointless code"

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+# References
+# https://www.youtube.com/watch?v=Pm938UxLEwQ [1]
+# https://stackoverflow.com/questions/70617258/session-object-in-fastapi-similar-to-flask [2]
+
 import os
 import httpx
 import json
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from dotenv import load_dotenv
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=os.urandom(32)) # [2]
 
 templates = Jinja2Templates(directory='templates')
 
@@ -16,13 +22,18 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 @app.get('/', response_class=HTMLResponse)
-def index(request: Request):
+async def index(request: Request):
+    if 'access_token' not in request.session.keys():
+        user = False
+    else:
+        user = True
     return templates.TemplateResponse(
-        request=request, name='index.html', context={'client_id': CLIENT_ID}
+        request=request, name='index.html', context={'client_id': CLIENT_ID, 'user': user}
     )
 
 @app.get('/callback')
-async def callback(code: str):
+async def callback(code: str, request: Request):
+    # [1] params and headers
     params = {
         'client_id':CLIENT_ID,
         'client_secret':CLIENT_SECRET,
@@ -30,15 +41,31 @@ async def callback(code: str):
     }
     headers = {'Accept': 'application/json'}
 
-    # Get Access token
+    # [1] Get Access token
     async with httpx.AsyncClient() as client:
         response = await client.post(url='https://github.com/login/oauth/access_token', params=params, headers=headers)
-    response_json = response.json()
-    headers['Authorization'] = f'Bearer {response_json["access_token"]}'
+        response_json = response.json()
+    access_token = response_json['access_token']
+    request.session['access_token'] = access_token
+    return RedirectResponse('/')
 
-    # Get starred repositories of the authorized user using access token
+@app.get('/github/starred')
+async def starred_data(request: Request):
+    if 'access_token' not in request.session.keys():
+        return RedirectResponse('/')
+
+    # [1] headers
+    headers = {'Accept': 'application/json', 'Authorization': f'Bearer {request.session["access_token"]}'}
+
+    # [1] Get all starred repositories
     async with httpx.AsyncClient() as client:
         response = await client.get('https://api.github.com/user/starred', headers=headers)
+
+    # If the access code has expired, delete saved data and redirect to home page
+    if response.status_code != 200:
+        del request.session['access_token']
+        return RedirectResponse('/')
+
     response_json = response.json()
     starred_data = convert_starred(response_json)
     return starred_data
